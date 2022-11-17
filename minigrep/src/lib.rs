9e9a3af83c2e66efa9da9cdc7f712ac907027538
Nicholas Sandomeno <nicholassandomeno@Nicholass-MBP.fios-router.home>
2022-11-17 13:29:54 -0500

    13.3

--- minigrep/src/lib.rs
+++ minigrep/src/lib.rs
@@ -16,19 +16,18 @@ pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
 }
 
 fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
-    let mut results = Vec::new();
-    for line in contents.lines() {
-        if line.contains(query) {
-            results.push(line);
-        }
-    }
-    results
+    // MUCH PRETTIER!
+    contents
+        .lines()
+        .filter(|line| line.contains(query))
+        .collect()
 }
 
 pub fn search_case_insensitive<'a>(
     query: &str,
     contents: &'a str,
 ) -> Vec<&'a str> {
+    // UGLY!
     let query = query.to_lowercase();
     let mut results = Vec::new();
     for line in contents.lines() {
@@ -46,15 +45,19 @@ pub struct Config {
 }
 
 impl Config {
-    pub fn build(args: &[String]) -> Result<Config, &'static str> {
-        if args.len() < 3 {
-            return Err("Must pass two arguments a string to search for, and a file to search in.")
-        }
-        let query = args[1].clone();
-        let file_path =args[2].clone();
-
+    pub fn build(
+        mut args: impl Iterator<Item = String>,
+    ) -> Result<Config, &'static str> {
+        args.next();
+        let query = match args.next() {
+            Some(arg) => arg,
+            None => return Err("didn't get a query string"),
+        };
+        let file_path = match args.next() {
+            Some(arg) => arg,
+            None => return Err("Didn't get a file path"),
+        };
         let ignore_case = env::var("IGNORE_CASE").is_ok();
-
         Ok(
             Config {
                 query,
--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -4,8 +4,7 @@ use minigrep::Config;
 
 
 fn main() {
-    let args: Vec<String> = env::args().collect();
-    let config = Config::build(&args).unwrap_or_else(|err| {
+    let config = Config::build(env::args()).unwrap_or_else(|err| {
         eprintln!("Problem parsing arguments: {err}");
         process::exit(1);
     });
@@ -13,7 +12,6 @@ fn main() {
         eprintln!("Application error: {e}");
         process::exit(1);
     }
-    //minigrep::run(config);
 }
 
 
